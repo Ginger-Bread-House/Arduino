40aa2dfc6f18b16598ad386318c8ed51e6975bb1
Ginger-Bread-House <162795259+Ginger-Bread-House@users.noreply.github.com>
2026-02-18 09:59:13 +0000

    git ---

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.getElementById("test").addEventListener("click", function() {
   this.style.color = "#87fe2c";
 });
+
+document.getElementById("speelKnop").addEventListener("click", function() {
+
+  fetch("http://localhost:3000/
+  .catch(error => console.error("Fout:", error));
+
+});start", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      command: "play"
+    })
+  })
+  .then(response => response.text())
+  .then(data => console.log("Server zegt:", data))
